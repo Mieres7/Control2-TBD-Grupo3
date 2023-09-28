8b3b254902aecb8f2d1e8c15e6e7809b22c82478
Mieres7 <vicente.mieres@usach.cl>
2023-09-27 21:39:39 -0300

    Creacion proyecto Frontend - vue. Inicializacion login.

---
+++ Frontend/src/components/Login.vue
@@ -0,0 +1,54 @@
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Login",
+});
+</script>
+
+<template>
+  <body>
+    <div class="form-card">
+      <div class="titles">aaaaaaaaaa</div>
+      <div class="form">bbbbbbbbbbbbb</div>
+      <div class="bot">ccccccccccccccc</div>
+    </div>
+  </body>
+</template>
+
+<style scoped>
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100vh;
+}
+
+/* Login component */
+
+.form-card {
+  display: grid;
+  grid-template-areas: "title", "form", "bot";
+  grid-template-columns: 100vh;
+  grid-template-rows: 20% 20% 60%;
+
+  height: 540px;
+  width: 400px;
+  border: solid red;
+  border-radius: 20px;
+  /* background-color: var(--white-bg); */
+}
+
+.titles {
+  grid-area: title;
+  background-color: red;
+}
+
+.form {
+  grid-area: form;
+}
+
+.bot {
+  grid-area: bot;
+}
+</style>
---
+++ Frontend/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Login from "../components/Login.vue";
+</script>
+
+<template>
+  <div class="effect">
+    <div class="center">
+      <Login />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* .center {
+  width: 100%;
+  height: 100vh;
+  backdrop-filter: blur(0.5rem);
+}
+
+.effect {
+  background-image: url("../views/icons/trees.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+} */
+</style>
